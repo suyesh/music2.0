6a2b783e6398b6acf3529da5b247e74e86637bbd
rhys2w <rhys@Rhyss-MacBook.local>
2013-07-25 19:30:36 -0400

    sign up done, sign in probs

--- music.rb
+++ music.rb
@@ -15,9 +15,21 @@ get '/home' do
 	haml :home
 end
 
-get '/profile' do
-	haml :profile
+post '/sign_in' do
+  @user = User.where(:username => params[:unsername]).first
+  if @user
+    if @user.password == params[:password]
+      redirect '/users/' + @user.id.to_s
+    end
+  end
 end
+
+post '/users/sign_up' do
+  User.create[params]
+  @users = User.all
+  redirect '/home'
+end
+
 
 get '/followers' do
 	haml :followers
@@ -25,4 +37,9 @@ end
 
 get '/following' do
 	haml :following
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  haml :profile
 end
